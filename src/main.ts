0ab6ea099b808ee3581146fd35f21a880b21c216
Tian <jacky841224j@gmail.com>
2024-10-10 18:30:37 +0800

    Feat新增Swagger功能

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+ //Swagger設定
+ const config = new DocumentBuilder()
+  .setTitle('Nest.js 爬蟲練習')
+  .setDescription('學習使用Nest.js寫爬蟲')
+  .setVersion('1.0')
+  .build();
+
+ const document = SwaggerModule.createDocument(app,config);
+ SwaggerModule.setup('api',app,document);
+
   await app.listen(3000);
 }
 bootstrap();
